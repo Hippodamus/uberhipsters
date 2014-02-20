93cbe44629bde53c837838d989c97acec2d33182
Mike Knudsen <mknudsen@alumni.nd.edu>
2014-02-19 16:14:56 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HippGrid::Application.config.secret_key_base = '5d863d7fd6ab60d49883129ca091978638e17f524244ca0e020eacd71e598fed6125f8857a337310ff23e518c2d40452353b3f495c10dce27a1588c3d8156f86'
