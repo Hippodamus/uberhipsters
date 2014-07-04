25c53b00ad34ee65ca793b03b78538b98be7bad9
Hippodamus <mknudsen@alumni.nd.edu>
2014-07-03 17:45:34 -0700

    trying to fix home routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ HippGrid::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about"
+  get "home" => "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
